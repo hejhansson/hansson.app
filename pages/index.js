c3b2f9d58e2b961d2ecd9224c8ae9562aded5b5a
hansson <alexander.hansson@bulldozer.se>
2019-04-29 20:45:09 +0200

    add: text and font

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import Head from '../components/Head'
+
 function Home() {
-  return <div>hnssn.se</div>;
+
+  return ( 
+  <div className="max-w-xl p-4 sm:p-12">
+    <Head/>
+    <p className="leading-tight mb-4 text-md sm:text-3xl">
+      Alexander Hansson<br/>
+      Designer och utvecklare
+    </p>
+
+    <p className="leading-tight mb-4 text-md sm:text-3xl">
+      Bor i Falkenberg och jobbar för Karlstadbaserade kommunikationsbyrån Bulldozer. Tar fram moderna webblösningar till nationella och internationella företag.
+    </p>
+
+    <p className="leading-tight mb-4 text-md sm:text-3xl">
+      Om du vill nå mig kan du göra det på:<br/>
+      alexander.hansson@bulldozer.se<br/>
+      +46 730 66 92 60
+    </p>
+  </div>
+  );
 }
 
 export default Home;
