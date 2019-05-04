d58a3744863cf0853f6bdfafb2041730d9d82c2d
hansson <alexander.hansson@bulldozer.se>
2019-05-04 09:47:50 +0200

    update: meta

--- components/Head.jsx
+++ components/Head.jsx
@@ -9,10 +9,7 @@ export default () => (
     <link href="/static/css/styles.css" rel="stylesheet"/>
     <link rel="icon" type="image/png" href="/static/favicon.png"/>
 
-    <meta property="og:url" content="https://kennetsplat.se" />
-    <meta property="og:image" content="/static/kennets-2.jpg" />
-    <meta property="og:description" content="Kennets plåt är din lokala plåtslagare i Falkenberg. Företaget startades av Kennet Larsson 1983 och togs över av Björn Benjaminsson 2010 och har fortsatt att drivas i samma anda där målsättningen är ett väl utfört arbete och med det nöjda kunder." />
-    <meta name="description" content="Kennets plåt är din lokala plåtslagare i Falkenberg. Företaget startades av Kennet Larsson 1983 och togs över av Björn Benjaminsson 2010 och har fortsatt att drivas i samma anda där målsättningen är ett väl utfört arbete och med det nöjda kunder."/>
+    <meta name="description" content="Designer och webbutvecklare som bor i Falkenberg och jobbar för Karlstadbaserade kommunikationsbyrån Bulldozer. Tar fram moderna webblösningar till nationella och internationella företag."/>
     
   </NextHead>
 )
