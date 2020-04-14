ff4f45edcd7bf15ab9a36ad1cca512ff2812c0af
alexander.hansson <alexander.hansson@glesys.se>
2020-04-14 16:10:27 +0200

    add lab grotesque

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -1,5 +1,5 @@
 export default () => (
-  <p className="leading-normal mb-4 text-lg sm:text-xl pt-6">
+  <p className="leading-normal mb-4 text-lg sm:text-2xl pt-6">
     <span className="block">alexander@hnssn.se</span>
     <span>+46702005625</span>
   </p>
--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 export default () => (
-  <h1 className="leading-tight font-normal text-lg sm:text-xl">
-    <Link href="/"><a className="text-black">Alexander Hansson<br/><span className="hidden sm:inline-block">designer & utvecklare</span></a></Link>
+  <h1 className="leading-tight font-normal text-lg sm:text-2xl">
+    <Link href="/"><a className="text-black">Alexander Hansson<br/><span className="">Designer & utvecklare</span></a></Link>
   </h1>
 )
