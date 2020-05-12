eeb10b1bdb9d8f2cf2d46de7c9e1b5c1d84480c8
alexander.hansson <alexander.hansson@glesys.se>
2020-05-12 09:03:11 +0200

    Add Space Grotesk

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 export default () => (
-  <h1 className="leading-tight font-normal text-lg sm:text-2xl">
+  <h1 className="leading-tight font-normal text-lg sm:text-2xl lg:text-3xl">
     <Link href="/"><a className="text-black">Alexander Hansson<br/><span className="">Designer & utvecklare</span></a></Link>
   </h1>
 )
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,11 @@ export default () => (
     <div className="p-4 sm:p-12">
       <Head/>
       <SiteHeader/>
-      <p className="leading-normal max-w-lg mb-4 pt-8 text-lg sm:text-2xl">
+      <p className="leading-normal max-w-xl mb-4 pt-8 text-lg sm:text-2xl lg:text-3xl">
         Bor i Falkenberg och jobbar som designer & utvecklare på <a target="_blank" className="border-b" href="https://glesys.se">GleSYS</a>.
         <br/>Pysslar främst med att vidareutveckla kontrollpanelen GleSYS Cloud. <br/>→ <a target="_blank" className="border-b" href="https://cloud.glesys.com/">https://cloud.glesys.com/</a>
       </p>
-      <p className="leading-normal max-w-lg mb-4 pt-4 text-lg sm:text-2xl">
+      <p className="leading-normal max-w-xl mb-4 pt-4 text-lg sm:text-2xl lg:text-3xl">
         Tidigare på kommunikationsbyrån Bulldozer där jag tog fram moderna webblösningar till nationella och internationella företag.
         Jobbat med varumärken som Brämhults, Löfbergs, Folktandvården Sverige, Byggcheferna och Hemfosa. 
       </p>
