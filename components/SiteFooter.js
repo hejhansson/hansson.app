69c54faa4bebf8829a72948c48cf10ef57b565d6
alexander.hansson <alexander.hansson@glesys.se>
2020-08-31 19:11:23 +0200

    update font sizes

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -1,5 +1,5 @@
 export default () => (
-  <p className="leading-normal mb-4 text-lg sm:text-xl lg:text-2xl pt-6">
+  <p className="leading-tight mb-4 text-lg sm:text-xl lg:text-3xl pt-6">
     <span className="block">alexander@hnssn.se</span>
     <span>+46702005625</span>
   </p>
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,11 @@ export default () => (
     <div className="p-4 sm:p-12">
       <Head/>
       <SiteHeader/>
-      <p className="leading-normal max-w-xl mb-4 pt-8 text-lg sm:text-xl lg:text-2xl">
+      <p className="leading-tight max-w-xl mb-4 pt-8 text-lg sm:text-xl lg:text-3xl">
         Bor i Falkenberg och jobbar som designer & utvecklare på <a target="_blank" className="with-background" href="https://glesys.se">GleSYS</a>.
         <br/>Pysslar främst med att vidareutveckla kontrollpanelen GleSYS Cloud.<br/> → <a target="_blank" className="with-background" href="https://cloud.glesys.com/">https://cloud.glesys.com/</a>
       </p>
-      <p className="leading-normal max-w-xl mb-4 pt-4 text-lg sm:text-xl lg:text-2xl">
+      <p className="leading-tight max-w-xl mb-4 pt-4 text-lg sm:text-xl lg:text-3xl">
         Tidigare på kommunikationsbyrån Bulldozer där jag tog fram moderna webblösningar till nationella och internationella företag.
         Jobbat med varumärken som Brämhults, Löfbergs, Folktandvården Sverige, Byggcheferna och Hemfosa. 
       </p>
