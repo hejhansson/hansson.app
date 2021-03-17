6de9577e37b36450312134d27b293aaa8c117f80
alexander.hansson <alexander.hansson@glesys.se>
2021-03-17 18:55:39 +0100

    fix spacing

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main className="p-4 xl:p-8 italic flex flex-col justify-between h-screen">
+      <main className="p-4 xl:p-8 italic xl:flex flex-col justify-between h-screen">
         <h1 className="text-lg xl:text-2xl max-w-2xl text-black">
           <span className="not-italic">ALEXANDER HANSSON</span> is a multi-disciplinary designer and developer based in Falkenberg, Sweden. Currently designing and developing at <a className="not-italic underline text-black hover:bg-yellow-100 hover:text-gray-900" href="https://glesys.se">GLESYS</a>. Formely at the ad agency <a className="not-italic underline text-black hover:bg-yellow-100 hover:text-gray-900" href="https://bulldozer.se">BULLDOZER</a>.
         </h1>
-        <h2 className="text-lg xl:text-xl max-w-xl leading-snug text-gray-800">
+        <h2 className="mt-8 xl:mt-0 text-lg xl:text-xl max-w-xl leading-snug text-gray-800">
           <a className="underline text-black hover:bg-yellow-100 hover:text-gray-900" href="mailto:alexander@hansson.app">alexander@hansson.app</a>
         </h2>
       </main>
